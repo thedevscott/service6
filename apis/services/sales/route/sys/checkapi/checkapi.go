3f8ca616675b14a38c2e46f3ae686c8ef7051a8c
Omar Scott <thedevscott@gmail.com>
2026-01-20 13:59:11 -0500

    commit 12: api check handlers added

---
+++ apis/services/sales/route/sys/checkapi/checkapi.go
@@ -0,0 +1,26 @@
+package checkapi
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func liveness(w http.ResponseWriter, r *http.Request) {
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+
+	json.NewEncoder(w).Encode(status)
+}
+
+func readiness(w http.ResponseWriter, r *http.Request) {
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+
+	json.NewEncoder(w).Encode(status)
+}
