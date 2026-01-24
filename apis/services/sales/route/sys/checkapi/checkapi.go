954edc3eb50d6947672d908248cf2fc2706d5852
Omar Scott <thedevscott@gmail.com>
2026-01-23 20:25:10 -0500

    commit 18: error handling updated

--- apis/services/sales/route/sys/checkapi/checkapi.go
+++ apis/services/sales/route/sys/checkapi/checkapi.go
@@ -2,8 +2,10 @@ package checkapi
 
 import (
 	"context"
+	"math/rand"
 	"net/http"
 
+	"github.com/ardanlabs/service/app/api/errs"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
@@ -18,6 +20,20 @@ func liveness(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 }
 
 func readiness(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	status := struct {
+		Status string
+	}{
+		Status: "OK",
+	}
+
+	return web.Respond(ctx, w, status, http.StatusOK)
+}
+
+func testError(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		return errs.Newf(errs.FailedPrecondition, "this message is trused")
+	}
+
 	status := struct {
 		Status string
 	}{
